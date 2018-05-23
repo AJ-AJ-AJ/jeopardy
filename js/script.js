4aaf29a235c9f56e77209f7de639eb8b5c6945d6
AJ-AJ-AJ <ajimenez@scmail.spelman.edu>
2018-05-23 14:42:32 -0400

    tenth commit

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ let ans27 = '';
 let ans28 = '';
 let ans29 = '';
 let ans30 = '';
-const listOfAnswers=[ans1,ans2,ans3,ans4,ans5];
+const listOfAnswers=[ans1,ans2,ans3,ans4,ans5,ans6];
 let score = 0;
 let scoreTally = 0;
 
